18ed1259effab7989940fe3225ac64d11a83cfb0
PCupboard <PatrickJemieljanczyk@gmail.com>
2024-10-07 10:50:15 +0200

    Gjorde ferdig 5.2 C og begynte på og gjorde ferdig helt 5.3 oppgaven

--- Obligatorisk oppgave 3/oppgave_5.py
+++ Obligatorisk oppgave 3/oppgave_5.py
@@ -71,6 +71,7 @@ print("\n")
 # Oppgave 5.2 - Mer funksjoner
 # Utvid forrige oppgave med noen funksjoner og benytt dem i koden din.
 
+
 # Deloppgave A
 print("Printer ut alle filmene med fin utskrift fra Oppgave 5.2 Deloppgave A:")
 def print_film_information(film_list_f):
@@ -96,5 +97,52 @@ average_rating = average_film_rating(film_list_f=film_list)
 print("Oppgave 5.2 Deloppgave B:")
 print(f"Gjennomsnits ratingen til alle filmene er: {round(average_rating, 2)}\n\n")
 
-# Deloppgave C
 
+# Deloppgave C
+def print_film_by_year(film_list_f, year_number):
+    release_year_list = []
+    for film_f in film_list_f:
+        if year_number <= film_f['year']:
+            release_year_list.append(film_f)
+
+    return release_year_list
+
+print("Skriv inn et heltall som skal være årstallet for alle filmene i listen fra og med det årstallet")
+while True:
+    user_ask_year = input("Årstall her: ")
+    if not user_ask_year.isdigit():
+        print("Dette er ikke et heltall!")
+        continue
+    else:
+        year_film_list = print_film_by_year(film_list_f=film_list, year_number=int(user_ask_year))
+        break
+
+# Bruker en tidligere funksjon for printe ut filmene
+print_film_information(year_film_list)
+print("\n\n")
+
+# -------------------------------------------------------------------------------------------------------
+
+# Oppgave 5.3 - Skrive/lese til fil
+# Utvid forrige oppgave med noen funksjoner og benytt dem i koden din
+
+
+# Deloppgave A
+def create_movie_file(film_list_f, file_name_f):
+    film_file = open(f"{file_name_f}" + ".txt", "w")
+    for film_f in film_list_f:
+        film_file.write(f"{film_f['name']} - {film_f['year']} has a rating of {film_f['rating']}\n")
+    film_file.close()
+
+
+user_file_name = input("Legg til et navn for filen som film listen skal printes ut til: ")
+
+create_movie_file(film_list_f=film_list, file_name_f=user_file_name)
+print("\n")
+
+# Deloppgave B
+def read_move_file(file_name_f):
+    film_file = open(f"{file_name_f}" + ".txt", "r")
+    print(film_file.read())
+
+read_move_file(file_name_f=user_file_name)
