bf4f4b3785f10219dba0f04333451b3b934f5ce9
PCupboard <PatrickJemieljanczyk@gmail.com>
2024-10-20 20:21:16 +0200

    Lagde klasser for spilleren og Dealeren, fortsatt usikker på hva disse klassene skal faktisk inneholde. Begge må ha en hit og stand. Kanskje Dealeren kan være en superclass og spilleren kan være childclass? Bygger dekket i dealer klassen for nå. Kanskje verdt å endre dette til sin egen klasse etterhvert? Har fått til å lage et dekk med 52 spillkort og klart å omstokke dekket til en tilfeldig rekkefølge. Kan da bare trekke kortene fra toppen av dekket (QOL yippi). Lagde en dictionary for chipsa til spilleren. Dette syntes jeg er dårlig implementert, må drøfte ideér for effektivisere dette. Alle spillerne får jo like mange chips i programmet mitt. (Kanskje endre dette også? :P)

--- Obligatorisk oppgave 4/Oppgave_2.py
+++ Obligatorisk oppgave 4/Oppgave_2.py
@@ -1,13 +1,34 @@
-
-suits = ['hearts', 'diamonds', 'spades', 'clubs']
-values = ['2', '3', '4', '5', '6', '7', '8', '9', 'king', 'queen', 'joker', 'ace']
+import random
 
 
-class DeckOfCards:
-    def __init__(self, value, suit):
-        self.value = value
-        self.suit = suit
+class Dealer:
+    def __init__(self) -> None:
+        self.suits = ['hearts', 'diamonds', 'spades', 'clubs']
+        self.ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'joker', 'queen', 'king', 'ace']
+        self.deck = []
 
-    def shuffle(self):
+
+    def build(self) -> None:
+        for suit in self.suits:
+            for rank in self.ranks:
+                self.deck.append((suit, rank))
+
+    def shuffle(self) -> None:
+        random.shuffle(self.deck)
+
+
+class Player:
+    def __init__(self):
+        self.chips = {"white": 20,
+                      "red": 20,
+                      "blue": 20,
+                      "green": 15,
+                      "black": 10,
+                      "pink": 5
+                     }
+
+    def hit(self):
         pass
 
+    def stand(self):
+        pass
