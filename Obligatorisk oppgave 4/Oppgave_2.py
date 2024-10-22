04d675604fbe9ba2c9fdc385840e263c007c42e9
PCupboard <PatrickJemieljanczyk@gmail.com>
2024-10-22 12:00:27 +0200

    Har begynt på Dealer klassen, gjorde ferdig koden for trekking av kort, og lagring av de trekte kortene og valuen på kortene tilsammen.

--- Obligatorisk oppgave 4/Oppgave_2.py
+++ Obligatorisk oppgave 4/Oppgave_2.py
@@ -1,6 +1,11 @@
 import random
 
 
+class Game:
+    def __init__(self):
+        pass
+
+# -------- DECK CLASS -------- #
 class Deck:
     def __init__(self) -> None:
         self.suits = ['hearts', 'diamonds', 'spades', 'clubs']
@@ -22,15 +27,16 @@ class Deck:
 
             else:
                 card.append(10)
+            print(card)
 
-        # Etter de to nesta for-løkkene er ferdig så eksisterer det en list of lists
+        # Etter de tre for-løkkene er ferdig så eksisterer det en list of lists
         # I hvert element i listen ligger det en liste som har 3 elementer i seg
         # 'suit', 'rank', og 'value'
         #  [0]      [1]        [2]      for å hente denne informasjonen
         #
         # EN EXCEPTION. 'ace' kortet har two values som en kan hente ved [2] og [3]
-
-            #print(card)
+        #                                                                 11  og 1 er valuen
+        # Happy drawing ;D
 
     def shuffle(self) -> None:
         random.shuffle(self.deck)
@@ -47,14 +53,14 @@ class Deck:
     def debug_draw_card(self) -> None:
         print(self.deck[:4])
 
-
+# -------- PLAYER CLASS -------- #
 class Player:
     def __init__(self, deck) -> None:
         self.deck = deck
         self.player_deck = []
 
-        self.chips = {"white": 20,
-                      "red": 20,
+        self.chips = {
+                      "white": 20,
                       "blue": 20,
                       "green": 15,
                       "black": 10,
@@ -65,15 +71,43 @@ class Player:
         self.player_deck.append(self.deck.draw())
         print(self.player_deck)
 
-
     def stand(self):
         pass
 
-
+# -------- DEALER CLASS -------- #
 class Dealer:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, deck) -> None:
+        self.deck = deck
+        self.dealer_deck = []
+        self.cards_value = 0
+        self.cards_count = -1
 
+    def hit(self) -> None:
+        self.dealer_deck.append(self.deck.draw())
+        self.cards_count += 1
+
+        current_card = self.dealer_deck[self.cards_count]
+        self.cards_value = self.cards_value + int(current_card[2])
+
+
+
+
+        print(current_card)
+        print(f"Dealer's hand value: {self.cards_value}")
+
+    def calculate(self) -> None:
+
+
+        if self.cards_value >= 17:
+            print("The dealer stands")
+
+        else:
+            pass
+
+    def kill_dealer(self) -> None:
+        self.dealer_deck.clear()
+        self.cards_count = 0
+        self.cards_value = 0
 
 
 your_deck = Deck()
@@ -81,12 +115,22 @@ your_deck.build()
 your_deck.shuffle()
 your_deck.debug_draw_card()
 
-player = Player(your_deck)
-player.hit()
-your_deck.debug_draw_card()
-player.hit()
-your_deck.debug_draw_card()
+dealer = Dealer(your_deck)
+dealer.hit()
+dealer.calculate()
+dealer.hit()
+dealer.calculate()
 
+#player = Player(your_deck)
+#player.hit()
+#your_deck.debug_draw_card()
+#player.hit()
+#your_deck.debug_draw_card()
+
+# Trenger å legge til en 'value' deck for både dealeren og spilleren
+# Denne vil lagre alle kortene i hånden til begge klassene og skrive ut verdien av hånden til spilleren og dealeren.
+# Viktig er å unngå duplikat av kort, slik at verdien kan bli hentet enkelt og greit
+#
 
 
 
